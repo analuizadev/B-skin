490cc10fed416c8812f820445af74911de79aee1
analuizadev <canaluizastro@gmail.com>
2023-01-05 21:29:19 -0300

    update

--- b-skin/src/components/services/Providers.js
+++ b-skin/src/components/services/Providers.js
@@ -1,42 +1,155 @@
-import Table from 'react-bootstrap/Table';
-
 import styles from './Providers.module.css';
+
+import {RxPencil1} from 'react-icons/rx'
+import {TiDeleteOutline} from 'react-icons/ti'
 
 function Providers() {
   return (
-    <div className={styles.body}>
-        <div className={styles.table}> 
-            <h1>Providers</h1>
-            <Table className={styles.table_container}>
+    <div className={styles.table}>
+
+        <table>
+            <caption>Providers</caption>
+            
+            <div className={styles.radius}>
                 <thead>
+                    <div className={styles.interactive}>
                     <tr>
-                    <th>Name</th>
-                    <th>Active</th>
-                    <th></th>
+                        <th>Name</th>
+                        <th>Created</th>
+                        <th>Active</th>
+                        <th></th>
                     </tr>
+                    </div>
                 </thead>
-                <tbody>
 
-                    <tr>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    </tr>
+                    <tbody>
+                    <div className={styles.scroll}>
+                        <tr>
+                            <td>Nike</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td><button><RxPencil1 /></button> <span><button><TiDeleteOutline /></button></span></td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Lv</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Farm</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Adidas</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Cantão</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
 
-                    <tr>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-
-                    <tr>
-                    <td colSpan={2}>Larry the Bird</td>
-                    <td>@twitter</td>
-                    </tr>
-
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                        <tr>
+                            <td>Amazon</td>
+                            <td>11/02/2021</td>
+                            <td>on</td>
+                            <td>botões</td>
+                        </tr>
+                    </div>
                 </tbody>
-            </Table>
-        </div>
+            </div>
+        </table>
+
     </div>
   )
 }
