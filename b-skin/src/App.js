dfe7f2b5e8ee476414b773fa92c122c93f743c5b
analuizadev <canaluizastro@gmail.com>
2023-01-09 22:36:34 -0300

    Dark mode

--- b-skin/src/App.js
+++ b-skin/src/App.js
@@ -1,6 +1,13 @@
-import './App.css';
-import React from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './components/others/Theme';
+import GlobalTheme from './components/others/Global';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import styles from './App.css'
+
+import {BsSun} from 'react-icons/bs';
+import {BsMoon} from 'react-icons/bs';
 
 import Home from './components/pages/Home';
 import News from './components/pages/News';
@@ -15,20 +22,59 @@ import Container from './components/layout/Container';
 import Footer from './components/layout/Footer';
 
 function App() {
+
+  const [theme, setTheme] = useState('light');
+    
+  const moon = document.getElementById('moon');
+  const sun = document.getElementById('sun');
+
+  const toggleDark = () => {
+
+    if (theme === "light"){
+      window.localStorage.setItem("theme", "dark");
+      setTheme("dark")
+      moon.classList.add('moon')
+      sun.classList.remove('sun')
+    }
+  }
+
+  const toggleLight = () => {
+    if (theme === "dark"){
+      window.localStorage.setItem("theme", "light");
+      setTheme("light")
+      sun.classList.add('sun')
+      moon.classList.remove('moon')
+    }
+  };
+
+  useEffect(() =>{
+    const localTheme = window.localStorage.getItem("theme");
+    localTheme && setTheme(localTheme);
+  }, [])
+
   return (
     <Router>
-      <NavBar />
-      <Container customClass="min_height">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/news" element={<News />}></Route>
-          <Route path="/shirt" element={<Shirt />}></Route>
-          <Route path="/shoppage" element={<ShopPage />}></Route>
-          <Route path="/providers" element={<Providers />}></Route>
-          <Route path="/order" element={<Order />}></Route>
-        </Routes>
-      </Container>
-      <Footer />
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <Fragment>
+          <GlobalTheme />
+          <div id="theme">
+            <button id="moon" onClick={toggleDark}><BsMoon /></button>
+            <button id="sun" onClick={toggleLight}><BsSun /></button>
+          </div>
+          <NavBar />
+          <Container customClass="min_height">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/news" element={<News />}></Route>
+              <Route path="/shirt" element={<Shirt />}></Route>
+              <Route path="/shoppage" element={<ShopPage />}></Route>
+              <Route path="/providers" element={<Providers />}></Route>
+              <Route path="/order" element={<Order />}></Route>
+            </Routes>
+          </Container>
+          <Footer />
+        </Fragment>
+      </ThemeProvider>
     </Router>
   );
 }
---
+++ b-skin/src/components/others/Theme.js
@@ -0,0 +1,8 @@
+export const lightTheme = {
+    body: "#f5f5f5"
+};
+
+export const darkTheme = {
+    body: "#141414",
+    text: "white"
+};
